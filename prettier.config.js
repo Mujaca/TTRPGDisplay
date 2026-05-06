9972e77b497347cbc80ab2f4e6099f6c3153c4e7
Jean-Michel <j.reupsch@mittwald.de>
2026-05-06 18:37:31 +0200

    added prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export const prettierConfigDefault = {
+    semi: true,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    proseWrap: "always",
+    tabWidth: 4,
+};
+
+export default prettierConfigDefault;
