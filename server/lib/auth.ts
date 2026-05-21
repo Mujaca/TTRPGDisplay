e79f9c947115ad4053998472a885ffac63a38df4
Jean-Michel <j.reupsch@mittwald.de>
2026-05-21 11:04:44 +0200

    added admin plugin

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,7 +1,7 @@
 import { prisma as client } from "./db";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { dash } from "@better-auth/infra";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
     database: prismaAdapter(client, { provider: "postgresql" }),
@@ -12,10 +12,10 @@ export const auth = betterAuth({
             clientId: process.env.DISCORD_CLIENT_ID!,
             clientSecret: process.env.DISCORD_CLIENT_SECRET!,
         },
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        },
+        //google: {
+        //    clientId: process.env.GOOGLE_CLIENT_ID!,
+        //    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        //},
     },
-    plugins: [dash()],
+    plugins: [admin()],
 });
