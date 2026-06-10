4c908159bcda15824dfbac101e8b813d2f098aa5
Mujaca <mujaca0@gmail.com>
2026-06-10 19:34:26 +0200

    added file cache

--- server/core/files/fileCache.ts
+++ server/core/files/fileCache.ts
@@ -1,4 +1,8 @@
 import NodeCache from "node-cache";
+import { s3, S3_BUCKET } from "~~/server/lib/s3";
+import { prisma } from "~~/server/lib/db";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
 
 export const fileCache = new NodeCache({
     stdTTL: 60 * 60, // 1 hour
@@ -7,3 +11,28 @@ export const fileCache = new NodeCache({
     deleteOnExpire: true,
     enableLegacyCallbacks: false,
 });
+
+export async function getFileUrl(fileId: string): Promise<string | null> {
+    const cachedUrl = fileCache.get<string>(fileId);
+    if (cachedUrl) {
+        return cachedUrl;
+    }
+
+    const file = await prisma.file.findUnique({
+        where: { id: fileId },
+    });
+
+    if (!file || file.fileType === "organisation/folder") {
+        return null;
+    }
+
+    const command = new GetObjectCommand({
+        Bucket: S3_BUCKET,
+        Key: file.path,
+    });
+
+    // @ts-expect-error
+    const url = await getSignedUrl(s3, command, { expiresIn: 60 * 60 }); // URL valid for 1 hour
+    fileCache.set(fileId, url);
+    return url;
+}
